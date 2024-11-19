2308ec0f484c3f4e95ba7b6bd4a46c037e775b0b
napling <matthewcellis98@gmail.com>
2024-11-18 22:43:49 -0600

    added comments to meet acceptance criteria

---
+++ main/routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+// get all users
+
+// get a single userr by its id
+
+// post a new user
+
+// put to upddate a user by its id
+
+// delete to remove a user, and remove its associated thoughts when deleted
+
+// /api/users/:userId/friends/:friendId post to add a new friend to a user's friend list
+
+// delete to remove a friend from a user's friend list
