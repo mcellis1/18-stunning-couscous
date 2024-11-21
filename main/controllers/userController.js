a6af177a73b8293f3d89810558e9994fa2115169
napling <matthewcellis98@gmail.com>
2024-11-20 18:19:10 -0600

    added post put and delete routes for users

--- main/controllers/userController.js
+++ main/controllers/userController.js
@@ -5,17 +5,19 @@ module.exports = {
 // get all users
     async getUsers(req, res) {
         try {
-            const users = await User.find()
+            const users = await User.find().select('-__v')
             res.json(users)
         } catch (err) {
             console.log(err)
             return res.status(500).json(err)
         }
     },
-// get a single userr by its id
+// get a single user by its id
     async getOneUser(req, res) {
         try {
-            const user = await User.findOne({ _id: req.params.userId }).select('-__v')
+            const user = await User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .populate('friends')
             if (!user) {
                 return res.status(404).json({ message: 'no student found with this id' })
             }
@@ -24,13 +26,45 @@ module.exports = {
             console.log(err)
             return res.status(500).json(err)
         }
-    }
+    },
 // post a new user
-
-// put to upddate a user by its id
-
+    async addUser(req, res) {
+        try {
+            const user = await User.create(req.body)
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+// put to update a user by its id
+    async editUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            ).select('-__v')
+            if (!user) {
+                res.status(404).json({ message: 'no user with this id found' })
+            }
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
 // delete to remove a user, and remove its associated thoughts when deleted
-
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId })
+            if (!user) {
+                res.status(404).json({ message: 'no user with this id found' })
+            }
+            await User.deleteMany({ _id: { $in: user.thoughts } })
+            res.json({ message: 'user and thoughts deleted' })
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    }
 // /api/users/:userId/friends/:friendId post to add a new friend to a user's friend list
 
 // delete to remove a friend from a user's friend list
--- main/routes/api/userRoutes.js
+++ main/routes/api/userRoutes.js
@@ -1,16 +1,23 @@
 const router = require('express').Router()
-const { getUsers, getOneUser } = require('../../controllers/userController')
+const {
+    getUsers,
+    getOneUser,
+    addUser,
+    editUser,
+    deleteUser
+} = require('../../controllers/userController')
 
 // get all users
 router.route('/').get(getUsers)
-// get a single userr by its id
-router.route('/:userId').get(getOneUser)
 // post a new user
+.post(addUser)
 
-// put to upddate a user by its id
-
+// get a single user by its id
+router.route('/:userId').get(getOneUser)
+// put to update a user by its id
+.put(editUser)
 // delete to remove a user, and remove its associated thoughts when deleted
-
+.delete(deleteUser)
 // /api/users/:userId/friends/:friendId post to add a new friend to a user's friend list
 
 // delete to remove a friend from a user's friend list
