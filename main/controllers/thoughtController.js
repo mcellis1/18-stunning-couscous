10f2906f33567185f0c66800f812c2664234fcb8
napling <matthewcellis98@gmail.com>
2024-11-21 18:59:12 -0600

    added comments to add the thoughts functions

---
+++ main/controllers/thoughtController.js
@@ -0,0 +1,13 @@
+// get all thoughts
+
+// get a single thought by its id
+
+// post to ccreate a neww thought dont forget to push the created thoughts id to the associated uiusers thoughts array field
+
+// put to update a thought by its id
+
+// delete to remove a thought by its id
+
+// /api/thoughts/:thoughtId/reactions post to create a reaction stored in a single thoughts reactions array field
+
+// delete to pull and remove a reaction by the readctions reactionid value
